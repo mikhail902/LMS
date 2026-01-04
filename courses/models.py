7344e2547cd55dac0feefbe4462ddd183fcea366
миша <misatoropkov@gmail.com>
2026-01-05 00:54:52 +0300

    all

---
+++ courses/models.py
@@ -0,0 +1,77 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class Course(models.Model):
+    """Модель курса"""
+
+    title = models.CharField(
+        _('title'),
+        max_length=200,
+        help_text=_('Название курса')
+    )
+
+    preview = models.ImageField(
+        _('preview image'),
+        upload_to='courses/previews/',
+        blank=True,
+        null=True,
+        help_text=_('Превью-изображение курса')
+    )
+
+    description = models.TextField(
+        _('description'),
+        help_text=_('Подробное описание курса')
+    )
+
+    class Meta:
+        verbose_name = _('course')
+        verbose_name_plural = _('courses')
+
+    def __str__(self):
+        return self.title
+
+
+class Lesson(models.Model):
+    """Модель урока"""
+
+    title = models.CharField(
+        _('title'),
+        max_length=200,
+        help_text=_('Название урока')
+    )
+
+    description = models.TextField(
+        _('description'),
+        help_text=_('Подробное описание урока')
+    )
+
+    preview = models.ImageField(
+        _('preview image'),
+        upload_to='lessons/previews/',
+        blank=True,
+        null=True,
+        help_text=_('Превью-изображение урока')
+    )
+
+    video_url = models.URLField(
+        _('video URL'),
+        blank=True,
+        null=True,
+        help_text=_('Ссылка на видео урока')
+    )
+
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.CASCADE,
+        related_name='lessons',
+        verbose_name=_('course'),
+        help_text=_('Курс, к которому принадлежит урок')
+    )
+
+    class Meta:
+        verbose_name = _('lesson')
+        verbose_name_plural = _('lessons')
+
+    def __str__(self):
+        return f"{self.title}"
---
+++ users/models.py
@@ -0,0 +1,66 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class User(AbstractUser):
+    """Модель пользователя с авторизацией по email"""
+    username = None
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        error_messages={
+            'unique': _("Пользователь с таким email уже существует."),
+        }
+    )
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message="Номер телефона должен быть в формате: '+999999999'. Максимум 15 цифр."
+    )
+    phone = models.CharField(
+        _('phone number'),
+        validators=[phone_regex],
+        max_length=17,
+        blank=True,
+        null=True
+    )
+    city = models.CharField(
+        _('city'),
+        max_length=100,
+        blank=True,
+        null=True
+    )
+
+    avatar = models.ImageField(
+        _('avatar'),
+        upload_to='users/avatars/',
+        blank=True,
+        null=True,
+        default='users/avatars/default.png'
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        """
+        Возвращает полное имя пользователя
+        """
+        full_name = f'{self.first_name} {self.last_name}'
+        return full_name.strip()
+
+    def get_short_name(self):
+        """
+        Возвращает короткое имя пользователя
+        """
+        return self.first_name
+
+
+
